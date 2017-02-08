9a8937a5d668e10b587ee69b5f2055d0eb698d00
eobaah <eobaah@gmail.com>
2017-02-07 16:37:14 -0800

    implemented create and edit modals

--- database.js
+++ database.js
@@ -5,10 +5,37 @@ const CONNECTION_STRING = `pg://${process.env.USER}@localhost:5432/sticky-egret-
 const db = pgp( CONNECTION_STRING )
 
 const getAllPosts = () =>
-  db.any( "SELECT * FROM blogPosts ORDER BY post_date ASC")
+  db.any( "SELECT * FROM blogPosts ORDER BY post_date ASC" )
 
 
+  const getPost = ( id ) =>
+      db.one( `SELECT * FROM blogPosts where id=$1`, [ id ] )
+
+  const createPost = ( { title, author, snippet, blog_entry, image_url, tags } ) =>
+    db.oneOrNone(
+      `INSERT INTO blogPosts
+         ( title, author, snippet, blog_entry, image_url, tags )
+      VALUES
+        ( $1, $2, $3, $4, $5, $6 )`,
+      [ title, author, snippet, blog_entry, image_url, tags ] )
+
+  const editPost = ( id, post ) => {
+    return db.oneOrNone( `UPDATE
+    blogPosts SET title=$2, author=$3, snippet=$4, blog_entry=$5, image_url=$6,
+    tags=$7  WHERE id=$1`,
+    [ id, post.title, post.author, post.snippet, post.blog_entry,
+    post.image_url, post.tags ] )
+  }
+
+  const deletePost = id =>
+    db.none( `DELETE FROM blogPosts WHERE id=$1`, [ id ] )
 
 
-
-module.exports = {db , getAllPosts}
+  module.exports = {
+    db,
+    getAllPosts,
+    getPost,
+    createPost,
+    editPost,
+    deletePost
+  }
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require( 'express' );
 const router = express.Router();
 const path = require( 'path' )
-const { getAllPosts } = require( '../database' )
+const { db, getAllPosts, getPost, createPost, editPost, deletePost } = require( '../database' )
 const moment = require( 'moment' )
 
 
@@ -20,5 +20,28 @@ router.get('/', function(req, res, next) {
 const normalizeDate = (date) =>{
   return moment(date).format('MMMM Do YYYY')
 }
+
+router.post( '/create', ( req, res, next ) => {
+  const post = req.body
+  createPost(post)
+    .then( () => res.redirect('/'))
+})
+
+router.post( '/delete/:id', ( req, res, next ) => {
+  const id = req.params.id
+  console.log(req.params.id)
+  deletePost( id )
+    .then( () => res.redirect( '/' ) )
+})
+
+router.post( '/edit/:id', ( req, res, next ) => {
+  const id  = req.params.id
+  const post  = req.body
+  editPost( id, post )
+  .then( () => {
+    res.redirect( '/' )
+  })
+})
+
 
 module.exports = router;
