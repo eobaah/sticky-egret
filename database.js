c6416ffc3e7a06958cd2b5e837fe8a3c6cfbd793
eobaah <eobaah@gmail.com>
2017-02-08 18:04:16 -0800

    fixed update functionality

--- database.js
+++ database.js
@@ -5,37 +5,38 @@ const CONNECTION_STRING = `pg://${process.env.USER}@localhost:5432/sticky-egret-
 const db = pgp( CONNECTION_STRING )
 
 const getAllPosts = () =>
-  db.any( "SELECT * FROM blogPosts ORDER BY post_date ASC" )
+  db.any( "SELECT * FROM posts ORDER BY post_date ASC" )
+
+const getPost = ( id ) =>
+  db.one( `SELECT * FROM posts where id=$1`, [ id ] )
+
+const createPost = ( { title, author, snippet, blog_entry, image_url, tags } ) =>
+  db.any(
+    `INSERT INTO posts
+       ( title, author, snippet, blog_entry, image_url, tags )
+    VALUES
+      ( $1, $2, $3, $4, $5, $6 )`,
+    [ title, author, snippet, blog_entry, image_url, tags ] )
 
 
-  const getPost = ( id ) =>
-      db.one( `SELECT * FROM blogPosts where id=$1`, [ id ] )
 
-  const createPost = ( { title, author, snippet, blog_entry, image_url, tags } ) =>
-    db.oneOrNone(
-      `INSERT INTO blogPosts
-         ( title, author, snippet, blog_entry, image_url, tags )
-      VALUES
-        ( $1, $2, $3, $4, $5, $6 )`,
-      [ title, author, snippet, blog_entry, image_url, tags ] )
+const editPost = ( id, post ) => {
+  return db.oneOrNone( `UPDATE
+  posts SET title=$2, author=$3, snippet=$4, blog_entry=$5, image_url=$6,
+  tags=$7  WHERE id=$1`,
+  [ id, post.title, post.author, post.snippet, post.blog_entry,
+  post.image_url, post.tags ] )
+}
 
-  const editPost = ( id, post ) => {
-    return db.oneOrNone( `UPDATE
-    blogPosts SET title=$2, author=$3, snippet=$4, blog_entry=$5, image_url=$6,
-    tags=$7  WHERE id=$1`,
-    [ id, post.title, post.author, post.snippet, post.blog_entry,
-    post.image_url, post.tags ] )
-  }
-
-  const deletePost = id =>
-    db.none( `DELETE FROM blogPosts WHERE id=$1`, [ id ] )
+const deletePost = id =>
+  db.none( `DELETE FROM posts WHERE id=$1`, [ id ] )
 
 
-  module.exports = {
-    db,
-    getAllPosts,
-    getPost,
-    createPost,
-    editPost,
-    deletePost
-  }
+module.exports = {
+  db,
+  getAllPosts,
+  getPost,
+  createPost,
+  editPost,
+  deletePost
+}
